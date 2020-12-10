5a6abaa1d8a43ebe51b25a35872952a21dcea07d
maridevmx <maridevmx@gmail.com>
2020-12-09 23:06:35 -0600

    feat: Introduction to jQuery

---
+++ jquery/efectos.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    let botonEfecto = document.getElementById('botonEfecto');
+    botonEfecto.addEventListener('click', efecto);
+
+    function efecto() {
+        // $(selector).efecto(velocidad, callback);
+        /* $('.caja').hide(3000, function () { 
+            alert('Oculto');
+         });
+
+         $('.caja').show(); */
+
+         // $('.caja').toggle(3000);
+         // $('.caja').fadeIn(1000);
+         // $('.caja').fadeOut(1000);
+         // $('.caja').fadeToggle(1000);
+
+         // $('.caja').slideDown();
+         // $('.caja').slideUp();
+         // $('.caja').slideToggle();
+    }
+});
---
+++ jquery/introduccion.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    
+    /* 
+        $ - Indicamos que vamos a acceder a jQuery.
+        selector - Accedemos al elemento que queramos trabajar.
+        accion - Lo que querermos hacer con el elemento seleccionado.
+    */
+    
+    $(selector).accion();
+
+
+});
