1f930d4c15ee5e0ce5b8d97141cfd77b1b13e514
maridevmx <maridevmx@gmail.com>
2020-12-14 18:47:48 -0600

    feat: Setting classes in jQuery

---
+++ jquery/trabajandoClass.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#boton').on('click', function(){
+        // $(this).addClass('naranja');
+        // $(this).removeClass('boton');
+
+        // $(this).toggleClass('naranja');
+
+        $(this).css({
+            'height': '100px',
+            'width': '100px'
+
+        });
+    });
+});
